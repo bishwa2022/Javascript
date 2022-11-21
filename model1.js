7ad71b4109e136d3631c92b599b304e63e80d0e1
bishwa2022 <bishwag@metropolia.fi>
2022-11-21 16:30:31 +0200

    model1 Exercise 1-3, html1

---
+++ model1.js
@@ -0,0 +1,21 @@
+'use strict';
+exercise 1
+console.log("I am printing to console")
+
+exercise 2
+let userName = prompt("Enter name");
+document.write("Hello " + userName);
+
+exercise 3
+let a = parseInt(prompt("Enter first number:"));
+let b = parseInt(prompt("Enter second number:"));
+let c = parseInt(prompt("Enter third number:"));
+
+let  sum = a + b + c;
+document.write('This sum is: ' + sum, "<br>");
+
+let product = a * b * c;
+document.write('This product is: ' + product, "<br>");
+
+let average = (a + b + c) / 3;
+document.write('The average is: ' + average);
